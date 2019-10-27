4b32ce12fe62098d38e7368c42b7facfc6e0ffa2
Yura-kenig <Yura-kenig@yandex.ru>
2019-10-27 10:53:06 +0300

    Модель photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_144455) do
+ActiveRecord::Schema.define(version: 2019_10_26_201307) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
